551c9eb74372f79578ce6eb50d09cfb3cf9cdbe6
Tanay Jalan <tanayjalan@gmail.com>
2013-12-01 20:01:26 -0500

    random changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131115040622) do
+ActiveRecord::Schema.define(version: 20131116060658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "highlights", force: true do |t|
+    t.integer  "issue_id"
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "issue_orders", force: true do |t|
     t.string   "email"
