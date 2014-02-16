c4173ec22a04461c7257b2f9ce94edad51e5b901
Lee Hampton <leejhampton@gmail.com>
2014-02-16 01:00:57 -0500

    add amount for issue charging

--- app/controllers/issue_orders_controller.rb
+++ app/controllers/issue_orders_controller.rb
@@ -6,7 +6,7 @@ class IssueOrdersController < ApplicationController
 
   def create
     charge = Stripe::Charge.create(
-      :amount => 50,
+      :amount => 3999,
       :currency => "usd",
       :card => params['issue_order']['stripe_card_token']
     )
