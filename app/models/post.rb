8f942bfadd007cfd056ade81beefc0eac141a0a8
jusjmkim <justinjmkim@gmail.com>
2014-10-26 03:02:35 -0400

    Modify ibr logo and add admin and post partials

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,20 @@
 class Post < ActiveRecord::Base
 
   def self.stories
-    find_posts('story')
+    Post.find_posts('story')
   end
 
   def self.features
-    find_posts('feature')
+    Post.find_posts('feature')
   end
 
   def self.editor_notes
-    find_posts('editor_notes')
+    Post.find_posts('editor_notes')
   end
 
   private
-    def find_posts(type)
-      posts = Post.find_by(type: type).all
-      return [posts[0], posts[1]]
+    def self.find_posts (type)
+      Post.find_by(type: type).all
     end
 
 end
