b8e767f29b31363cb2da0b869eac85bb568b660d
jusjmkim <justinjmkim@gmail.com>
2014-10-30 22:52:55 -0400

    initial posts controller setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,17 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
 
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @post }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -17,11 +27,28 @@ class PostsController < ApplicationController
   end
 
   def update
-
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to '/online' }
+      format.json { head :no_content }
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :author, :body, :story_type)
+    end
 
 end
