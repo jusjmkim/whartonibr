c51d0c4608fcf94d943b6eef864762edb3d712da
jusjmkim <justinjmkim@gmail.com>
2014-11-10 01:55:40 -0500

    Got modals working

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function showPostListener() {
+  $('a.post-preview').click(function(e) {
+    e.preventDefault();
+    var id = this.id;
+    displayModal(id);
+  });
+}
+
+function displayModal(id) {
+  $('#modal-content' + id).modal('show');
+}
+
+$(function() {
+  showPostListener();
+});
