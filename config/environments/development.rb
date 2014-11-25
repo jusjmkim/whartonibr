d8b42b4c649601c161f448191b13158758a01cd4
jusjmkim <justinjmkim@gmail.com>
2014-11-25 15:10:52 -0500

    move configuration to environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Ibr::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   
   # Paperclip for PDF uploads
   Paperclip.options[:command_path] = "/usr/local/bin/"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Ibr::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # Generate digests for assets URLs.
   config.assets.digest = true
