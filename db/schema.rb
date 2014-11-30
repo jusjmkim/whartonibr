48257a4b13cb5a0209f2d95dd6e2e2bda199ccdb
jusjmkim <justinjmkim@gmail.com>
2014-11-30 17:29:35 -0500

    Delete unnecessary compiled files and recompile all files. Add gotham bold and medium fonts

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20141125203009) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "author"
-    t.text     "body",               limit: 255
+    t.text     "body"
     t.string   "story_type"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -74,23 +74,5 @@ ActiveRecord::Schema.define(version: 20141125203009) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
-
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
