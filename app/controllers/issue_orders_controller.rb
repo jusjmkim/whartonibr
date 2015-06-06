a20a7fd575d6112240c659382cd333d22a181377
jusjmkim <justinjmkim@gmail.com>
2015-06-06 15:21:35 -0700

    take out stripe

--- app/controllers/issue_orders_controller.rb
+++ app/controllers/issue_orders_controller.rb
@@ -5,33 +5,19 @@ class IssueOrdersController < ApplicationController
   end
 
   def create
-    charge = Stripe::Charge.create(
-      :amount => 399,
-      :currency => "usd",
-      :card => params['issue_order']['stripe_card_token']
-    )
-    if charge['paid'] == true
-      @issue_order = IssueOrder.new(email: params['issue_order']['email'], issue_id: params['issue_id'])
-      if @issue_order.save
-        @pdf_token = @issue_order.pdf_token
-        PdfMailer.pdf_email(params['issue_order']['email'], @issue_order).deliver
+    @issue_order = IssueOrder.new(email: params['issue_order']['email'], issue_id: params['issue_id'])
+    if @issue_order.save
+      @pdf_token = @issue_order.pdf_token
+      PdfMailer.pdf_email(params['issue_order']['email'], @issue_order).deliver
 
-        respond_to do |format|
-          format.html { redirect_to root, notice: 'Issue was successfully bought.' }
-        end
-      else 
-        flash[:error] = []
-        @issue_order.errors.full_messages.each do |error_message|
-          flash[:error].push(error_message)
-        end
-        respond_to do |format|
-          format.js
-        end
+      respond_to do |format|
+        format.html { redirect_to '/', notice: 'Issue was successfully bought.' }
       end
     else
-      # run checks for errors and return error message
       flash[:error] = []
-      flash[:error].push(charge['failure_message'])
+      @issue_order.errors.full_messages.each do |error_message|
+        flash[:error].push(error_message)
+      end
       respond_to do |format|
         format.js
       end
