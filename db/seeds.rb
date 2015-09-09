7bfc45e81dd00100a49f074e65205eed33fe0697
Gina <gliu96@gmail.com>
2015-09-09 19:14:52 -0400

    updated board profiles, added Fall 2014 issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,7 @@ issue_3 = Issue.create(name: "Winter 2012", title: "Technology: What's Next?", i
 issue_4 = Issue.create(name: "Spring 2013", title: "The Debt Nexus", issue_img: "Spring 2013.jpg", volume: 6)
 issue_5 = Issue.create(name: "Autumn / Winter 2013", title: "Luxury", issue_img: "Fall 2013.jpg", volume: 7)
 issue_6 = Issue.create(name: 'Spring / Summer 2014', title: 'Defining Leadership', issue_img: 'Spring 2014.png', volume: 8)
+issue_7 = Issue.create(name: "Autumn / Winter 2014", title: "The Investment Pitchbook", issue_img: 'Fall 2014.png', volume: 9)
 
 highlight_0 = Highlight.create(issue_id: issue_0.id,
                               title: "Interview with Jeremy Siegel, Professor of Finance, Wharton School Interviewed by
@@ -110,3 +111,9 @@ highlight_11 = Highlight.create(issue_id: issue_6.id,
 highlight_12 = Highlight.create(issue_id: issue_6.id,
                                 title: "A View of Leadership from Behind the Lens with Photographer Platon Antoniou",
                                 description: "Platon: \"People in power are realizing that they have to come up with new models of leadership. They have to readdress their core values...Compassion has not been part of the equation of being a good leader... in today’s society, that has to be the case now.\"")
+highlight_13 = HighLight.create(issue_id: issue_7.id,
+                                title: "High Altitude, High Yield: Sovereign Debt in Jaguar Economies",
+                                description: "In a world of zero interest rates and poor fixed income performance, the debt of certain Latin American countries like Peru and Colombia merit a closer look due to strong economic performance stemming from governmental and fiscal reforms.")
+highlight_14 = HighLight.create(issue_id: issue_7.id,
+                                title: "IBR Conversation with Yie-Hsin Hung",
+                                description: "Yie-Hsin Hung: \"The long-term nature of the investment management business suggests that all efforts take time to realize...The core value proposition of the business is to deliver excess returns for investors.\"")
